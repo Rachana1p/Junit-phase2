d3e12741f5567cb51044061f717b8fbf174ea3b2
rachana1p <rachanaprakash2001@gmail.com>
2023-12-29 18:49:30 +0530

    second commit

--- src/test/java/unittest/RetirementCalculatorTest.java
+++ src/test/java/unittest/RetirementCalculatorTest.java
@@ -1,5 +1,20 @@
 package unittest;
 
-public class RetirementCalculatorTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
+public class RetirementCalculatorTest {
+AgeCalculator ageCalculator;
+	
+	@BeforeEach
+	public void setUp() {
+		ageCalculator  = new AgeCalculator();
+	}
+
+	@AfterEach
+	public void cleanUp() {
+		if(ageCalculator!=null)
+			ageCalculator = null;
+	}
+	
 }
